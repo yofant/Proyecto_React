1477659598e898ef1c64c9d513234e3c3b43e363
GiovanniSierraCun <fredy_sierra@cun.edu.co>
2025-04-03 21:33:24 -0500

    REACT  Y  BOSSTRAP

---
+++ src/IndexApp.jsx
@@ -0,0 +1,20 @@
+import "./IndexApp.css"
+import "./NavBar"
+import NavBar from "./NavBar";
+
+//funcion flechitas
+const IndexApp = () => {
+
+    return(
+        <>
+        <NavBar/>
+        <h1 className="saludo" >Bienvenido</h1>
+        <p>Clase  de  Desarrollo </p>
+
+        </>
+    );
+
+    
+
+}
+export default IndexApp;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import IndexApp from  "./IndexApp"
+
+
+const root = ReactDOM.createRoot (document.getElementById('root'));
+root.render (
+
+<IndexApp/>
+
+);
